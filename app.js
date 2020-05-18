1bc7aedc0fbe386def0ef42635262e40d63fc3eb
Panarat D <panarat.duke@gmail.com>
2020-05-18 01:11:02 -0500

    Add demographic Info

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
-function unpack(rows,index){
-    return rows.map(function(row){
-        return row[index];
-    });
-}
+// Return result from dropDown
+console.log("Refresh 10");
 
 
-dropDown=d3.select('#select-otuId');
-d3.json('Resources/samples.json').then((data)=>{
-    console.log("Print Metadata",data.metadata);
-    var info = data.metadata;
-    console.log("Print info", info)
-    // var id = info.map();
-    // console.log("Print id = ", id)
-    
-    // console.log("Print id",info);
-    var sampleNames=data.names;
-    sampleNames.forEach((sample)=>{
-        dropDown.append('option')
-                .text(sample)
-                .property('this.value', sample);
-    });
-});
-
-// const VERSION: number = 2016030600;
-// export default VERSION;
-
-function changedOption(optionSelected){
-    console.log("changedOption : optionSelected = ",optionSelected);
-    // var demographicInfo = 
-    var printDemographic=d3.select('#demographic');
-    printDemographic.text(
-        `ID : ${optionSelected}`);
+function changedOption(selectOption){
+    // d3.event.preventDefault();
+    console.log("changedOption : selectOption = ",selectOption);
+    var addText = d3.select("#demographic");
+    console.log("addText before: ",addText)
+    addText.html=""; 
+    demographicInfo(selectOption);
 };
 
-// init();
+function demographicInfo(selectOption){
+    // d3.event.preventDefault();
+    var addText = d3.select("#demographic");
+    console.log("addText before: ",addText)
+    addText.html=""; 
+    console.log("addText after remove: ",addText)
+    d3.json('Resources/samples.json').then((data)=>{ 
+        
+        var demoInfo = data.metadata.filter(row => row.id == selectOption);
+        // console.log("Print demographicInfo : demoInfo= ",demoInfo)
+        Object.entries(demoInfo[0]).forEach(([key,value])=>{
+            // console.log("Print Object.entries : ",[key,value])       
+            addText.append('p').text(`${key} : ${value}`);
+        });
+    });
+
+}
+    
+function init(){
+    //Import file from json
+    d3.json('Resources/samples.json').then((data)=>{
+    
+        var sampleNames=data.names;
+       
+        var dropDown=d3.select('#select-otuId');
+        sampleNames.forEach((sample)=>{
+            dropDown.append('option')
+                    .text(sample)
+                    .property('this.value', sample);
+        });
+        
+    });
+    demographicInfo(941);
+}
+init();
